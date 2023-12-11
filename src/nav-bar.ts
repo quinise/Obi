63f3330c8e9dc22ace3da73e4ef271c3536ecbb8
quinise <quinise.ercolano@gmail.com>
2023-12-10 18:48:35 -0800

    test

--- src/nav-bar.ts
+++ src/nav-bar.ts
@@ -60,7 +60,6 @@ class NavBar extends LitElement {
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                             </svg>  
                         </button>
-
                     </div>
                 </nav>
             </header>
